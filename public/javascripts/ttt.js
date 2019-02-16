811b9fbc833978cc4ab26cc4c1a2aee247b0daa8
Isaac Duarte <isaacpd@yahoo.com>
2019-02-16 11:39:02 -0500

    jquery and ttt js

---
+++ public/javascripts/ttt.js
@@ -0,0 +1,73 @@
+let player = 'O';
+let turn = true;
+
+let game = {
+	grid: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	winner: ''
+};
+
+let check_win = function () {
+	if (game.winner !== '') return;
+	let inARow = 0, inACol = 0, diag = 0, rdiag = 0;
+
+	for (let i = 0; i < 3; i++) {
+		if (game.grid[3 * i + i] === player) diag++;
+		if (game.grid[3 * i + 2 - i] === player) rdiag++;
+
+		for (let j = 0; j < 3; j++) {
+			if (game.grid[3 * i + j] === player) inARow++;
+			if (game.grid[3 * j + i] === player) inACol++;
+
+			if (inARow === 3 || inACol === 3) {
+				game.winner = player;
+				return;
+			}
+		}
+		inACol = 0; inARow = 0;
+	}
+	if (diag === 3 || rdiag === 3)
+		game.winner = player;
+};
+
+let c_move = function (res) {
+	game = JSON.parse(res);
+	for (let row = 0; row < 3; row++) {
+		for (let col = 0; col < 3; col++) {
+			let cell = $("#row-" + row).find(".c-" + col);
+			cell.text(game.grid[3 * row + col])
+		}
+	}
+	check_win();
+	turn = true;
+};
+
+let p_move = function (row, col) {
+	if (game.grid[3 * row + col] !== ' ' || !turn || game.winner !== '') return;
+	game.grid[3 * row + col] = player;
+	check_win();
+
+	let cell = $("#row-" + row).find(".c-" + col);
+	cell.text(player);
+
+	turn = false;
+	$.post('ttt/move', JSON.stringify(game), c_move, 'json');
+	console.log(game.grid)
+};
+
+let load_game = function () {
+	let board = $(".board");
+	board.append("what's up");
+	console.log("hello");
+	for (let i = 0; i < 3; i++) {
+		let cl = i !== 2 ? "border-bottom border-dark" : "";
+		board.append("<div class='row " + cl + "' id='row-" + i + "'></div>");
+		var row = $('#row-' + i);
+		for (let j = 0; j < 3; j++) {
+			cl = j !== 2 ? "border-right border-dark" : "";
+			row.append("<div class='square col " + cl + " c-" + j + "' onclick='p_move(" + i + ", " + j + ")'></div>")
+		}
+	}
+	console.log("hello")
+};
+
+$(document).ready(load_game);
