401c515f8440ea42b2164fe27724515bf1083cf5
limboaz <limboaz@outlook.com>
2019-02-27 11:25:26 -0500

    Testing send email

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('express-handlebars');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var tttRouter = require('./routes/ttt');
@@ -18,6 +25,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//handle session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*
+//validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg : msg,
+            value : value
+        };
+    }
+}));
+*/
 app.use('/', indexRouter);
 app.use('/ttt', tttRouter);
 
--- routes/ttt.js
+++ routes/ttt.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express.Router();
 var __dir = 'public';
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
@@ -55,19 +56,25 @@ app.get(/(javascripts)|(stylesheets)/, function(req, res, next) {
 
 // TODO Warm Up Project 2
 app.post('/adduser', function(req, res){
-
+    let user = req.body; // username, password, email
+    console.log(user);
+    send_verify_mail(user.username, user.email).then(function(){
+        console.log("Yay");
+    });
 });
 
 app.post('/verify', function(req, res){
-
+    let email = req.body.email;
+    let key = req.body.key;
 });
 
 app.get('/verify', function(req, res){
-
+    let email = req.query.email;
+    let key = req.query.key;
 });
 
 app.post('/login', function(req, res){
-
+    let user = req.body; //username, password
 });
 
 app.post('/logout', function(req, res){
@@ -79,11 +86,36 @@ app.post('/listgames', function(req, res){
 });
 
 app.post('/getgame', function(req, res){
-
+    let id = req.body.id;
 });
 
 app.post('/getscore', function(req, res){
 
 });
+
+// TODO Send Email {Currently getting "Error: connect ECONNREFUSED 127.0.0.1:3001"}
+// TODO Send Email {Currently getting "}
+async function send_verify_mail(username, email){
+    let account = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: "localhost",//"152.44.36.183",
+        port: 3000,
+        secure: false,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    });
+
+    let mailOptions = {
+        from: '"Tic Tac Toe Server" <auth@localhost>',
+        to: email,
+        subject: "Verify your Tic Tac Toe account",
+        text: "Click on this link to verify your account https://152.44.36.183/ttt/verify?email=" + email + "&key=abracadabra"
+    };
+
+    let info = await transporter.sendMail(mailOptions);
+}
 
 module.exports = app;
