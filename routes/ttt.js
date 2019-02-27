4950e2308faae53d1a7dce469728cdbb68a78f0c
Yinuo Zhang <limboaz@outlook.com>
2019-02-27 17:32:23 -0500

    successful in sending email

--- routes/ttt.js
+++ routes/ttt.js
@@ -55,11 +55,33 @@ app.get(/(javascripts)|(stylesheets)/, function(req, res, next) {
 });
 
 // TODO Warm Up Project 2
+
+
 app.post('/adduser', function(req, res){
-    let user = req.body; // username, password, email
+    let user = req.body.username; // username, password, email
     console.log(user);
-    send_verify_mail(user.username, user.email).then(function(){
-        console.log("Yay");
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.mail.yahoo.com',
+        port: 465,
+        service: 'yahoo',
+        secure: false,
+        auth: {
+            user: 'tictactoeppg@yahoo.com',
+            pass: 'helloppg2019'
+        }
+    });
+    let mailOptions = {
+        from: 'tictactoeppg@yahoo.com',
+        to: req.body.email,
+        subject: 'Verifying your Tic Tac Toe account',
+        text: '"Click on this link to verify your account https://152.44.36.183/ttt/verify?email=" + email + "&key=abracadabra"'
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
     });
 });
 
@@ -93,29 +115,5 @@ app.post('/getscore', function(req, res){
 
 });
 
-// TODO Send Email {Currently getting "Error: connect ECONNREFUSED 127.0.0.1:3001"}
-// TODO Send Email {Currently getting "}
-async function send_verify_mail(username, email){
-    let account = await nodemailer.createTestAccount();
-
-    let transporter = nodemailer.createTransport({
-        host: "localhost",//"152.44.36.183",
-        port: 3000,
-        secure: false,
-        auth: {
-            user: account.user,
-            pass: account.pass
-        }
-    });
-
-    let mailOptions = {
-        from: '"Tic Tac Toe Server" <auth@localhost>',
-        to: email,
-        subject: "Verify your Tic Tac Toe account",
-        text: "Click on this link to verify your account https://152.44.36.183/ttt/verify?email=" + email + "&key=abracadabra"
-    };
-
-    let info = await transporter.sendMail(mailOptions);
-}
 
 module.exports = app;
