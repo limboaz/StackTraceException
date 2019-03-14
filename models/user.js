be77bc8cfff4306acb87bba252365c448370baad
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-03-14 11:23:07 -0400

    added verification to user model: username (unique, must be a-zA-Z0-9, can't be blank), email: (can't be blank, must be x@x.x)

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	username: {type: String, index: true},
+	username: {type: String, index: true, unique: true, required: [true, "User name can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'This username is invalid, please use only letters a-z, A-Z, or digits 0-9']},
 	password: String,
 	sid: String,
-	email: {type: String, index: true},
+	email: {type: String, index: true, required: [true, "Email can't be blank"], match: [/\S+@\S+\.\S+/, 'Email you entered is invalid, please use proper format example@somewhere.com ']},
 	enabled: {type: String, default: "False"},
 });
 
