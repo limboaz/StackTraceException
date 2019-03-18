aa64acc73967b21339aef0a00542bdb19a657b97
Isaac <isaacpd@yahoo.com>
2019-03-18 15:43:02 -0400

    update models

--- models/question.js
+++ models/question.js
@@ -2,24 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
-    id: String,
+    id: {type: String, index: true},
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
+        index: true
     },
     answers: [{type: Schema.Types.ObjectId, ref: 'Answer'}],
-    title: String,
-    body: String,
-    score: Number,
-    view_count: Number,
-    answer_count: Number,
+    title: {type: String, required: true},
+    body: {type: String, required: true},
+    score: {type: Number, default: 0},
+    view_count: {type: Number, default: 0},
+    answer_count: {type: Number, default: 0},
     timestamp: {
-        type: Date,
+        type: Number,
         default: Date.now() / 1000 // Date in unix time, Date.now returns in milliseconds so need seconds ID
     },
     media: [Number],
     tags: [String],
-    accepted_answer_id: Number
+    accepted_answer_id: {type: Schema.Types.ObjectId, ref: 'Answer'}
 
 });
 
