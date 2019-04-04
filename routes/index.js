73081e2e92385410ed20ac1724da9bb9c50d2fd2
Isaac <isaacpd@yahoo.com>
2019-04-04 15:29:29 -0400

    add request package for use in frontend

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
-const User = require('../models/user');
-const Question = require('../models/question');
-const mongoStore = require('../mongoose');
+const request = require('request');
 const router = express.Router();
 const __dir = 'public';
 
-router.get('/:id', function(req, res){
-	Question.findOne({id: req.params.id}).
-		populate({path: 'user', select: 'username'}).
-		exec(function(err, question){
-			if (err || !question){
-				return res.render('error');
-			}
-			res.render('question', {question: question, user_link: '/user/' + question.user.username + '/questions'})
-		});
+router.get('/quest/:id', function (req, res) {
+	request.get('http://localhost:3000/questions/' + req.params.id, function (err, resp, body) {
+		let b = JSON.parse(body);
+		if (err || b.status !== 'OK') {
+			return res.render('error');
+		}
+		let question = b.question;
+		res.render('question', {question: question, user_link: '/user/' + question.user.username + '/questions'})
+	});
 });
 
-router.get(/.*\..*/, function(req, res){
+router.get(/.*\..*/, function (req, res) {
 	console.log(req.path);
 	res.status(404);
 	res.send('Not found');
