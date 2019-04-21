14e98531fd6d2d8c71e653abedec87b840ea0b62
Isaac <isaacpd@yahoo.com>
2019-04-20 23:05:04 -0400

    Include new cassandra node

--- cassandra.js
+++ cassandra.js
@@ -1,7 +1,7 @@
 const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({
-    contactPoints:['127.0.0.1'],
+    contactPoints:['10.3.5.14'],
     keyspace: 'ste',
     localDataCenter: 'datacenter1'
 });
