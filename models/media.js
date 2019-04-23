390653b89c6ea263e688ef68b09166d4366d4854
Isaac <isaacpd@yahoo.com>
2019-04-23 08:02:51 -0400

    remove unique for media id

--- models/media.js
+++ models/media.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const mediaSchema = new Schema({
-	_id: {type: String, unique: true},
+	_id: String,
 	used: {type: Boolean, default: false}
 });
 
