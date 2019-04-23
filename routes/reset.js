56f11f03255032bd4ce4cb9c334784ccc51702ec
Isaac <isaacpd@yahoo.com>
2019-04-23 02:37:21 -0400

    reverse order for reset

--- routes/reset.js
+++ routes/reset.js
@@ -6,24 +6,24 @@ const Question = require('../models/question');
 const User = require('../models/user');
 
 router.get('/reset', function (req, res) {
-    let query = "TRUNCATE ?";
-    cassandra.execute(query, "media",{prepare: true},function (err, result) {
-        if(err)
-            return res.status(400).json({status: "error", error: "Error truncating cassandra"});
-        Answer.deleteMany({}, function (err, result) {
-            if(err)
-                return res.status(400).json({status: "error", error: "Error removing Mongo Collection: Answer"});
-            Question.deleteMany({}, function (err, result) {
-                if(err)
-                    return res.status(400).json({status: "error", error: "Error removing Mongo Collection: Question"});
-                User.deleteMany({}, function (err, result) {
-                    if(err)
-                        return res.status(400).json({status: "error", error: "Error removing Mongo Collection: User"});
-                    res.json({status: "OK", message:"Successfully deleted everything from database."})
-                })
-            })
-        });
-    });
+	Answer.deleteMany({}, function (err, result) {
+		if (err)
+			return res.status(400).json({status: "error", error: "Error removing Mongo Collection: Answer"});
+		Question.deleteMany({}, function (err, result) {
+			if (err)
+				return res.status(400).json({status: "error", error: "Error removing Mongo Collection: Question"});
+			User.deleteMany({}, function (err, result) {
+				if (err)
+					return res.status(400).json({status: "error", error: "Error removing Mongo Collection: User"});
+				let query = "TRUNCATE ?";
+				cassandra.execute(query, "media", {prepare: true}, function (err, result) {
+					if (err)
+						return res.status(400).json({status: "error", error: "Error truncating cassandra"});
+					res.json({status: "OK", message: "Successfully deleted everything from database."})
+				})
+			})
+		});
+	});
 });
 
 module.exports = router;
