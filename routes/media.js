dd8f2dbfdce685dc82ccf5cc17826bf5f75c90cf
Isaac <isaacpd@yahoo.com>
2019-04-24 21:13:38 -0400

    check if rows length is 0

--- routes/media.js
+++ routes/media.js
@@ -7,35 +7,31 @@ const shortID = require('shortid');
 let upload = multer();
 
 router.post('/addmedia', upload.single("content"), function (req, res) {
-    if (!req.session.userId)
-        return res.status(404).json({status: "error", error: "User not logged in."});
-    let uid = shortID.generate();
-    let query = 'INSERT INTO media (id, content, type) VALUES (?, ?, ?)';
-    let params = {
-        id : uid,
-        content: req.file.buffer,
-        type : req.file.mimetype
-    };
-    cassandra.execute(query, params, {prepare: true}, function(err, result){
-        if(err)
-            return res.status(404).json({status: "error", error: err.toString()});
-        res.json({status: "OK", id: uid});
-        let m = new Media({_id: uid, user: req.session.userId});
-        m.save();
-    });
+	if (!req.session.userId)
+		return res.status(404).json({status: "error", error: "User not logged in."});
+	let uid = shortID.generate();
+	let query = 'INSERT INTO media (id, content, type) VALUES (?, ?, ?)';
+	let params = {
+		id: uid,
+		content: req.file.buffer,
+		type: req.file.mimetype
+	};
+	cassandra.execute(query, params, {prepare: true}, function (err, result) {
+		if (err)
+			return res.status(404).json({status: "error", error: err.toString()});
+		res.json({status: "OK", id: uid});
+		let m = new Media({_id: uid, user: req.session.userId});
+		m.save();
+	});
 });
 
 router.get('/media/:id', function (req, res) {
-    let query = 'SELECT content, type FROM media WHERE id = ?';
-    Media.find({_id: req.params.id}, function (err, media) {
-        if (err || !media)
-            return res.status(404).json({status: "error", error: err ? err.toString() : "media does not exist"});
-        cassandra.execute(query, [req.params.id],function(err, result){
-            if(err || !result)
-                return res.status(404).json({status: "error", error: err.toString()});
-            res.type(result.rows[0].type).send(result.rows[0].content);
-        });
-    });
+	let query = 'SELECT content, type FROM media WHERE id = ?';
+	cassandra.execute(query, [req.params.id], function (err, result) {
+		if (err || !result || result.rows.length === 0)
+			return res.status(404).json({status: "error", error: err.toString()});
+		res.type(result.rows[0].type).send(result.rows[0].content);
+	});
 });
 
 module.exports = router;
