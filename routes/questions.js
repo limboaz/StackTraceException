6f53e22f8f440b3647622330ba175052d1be4a8e
Isaac <isaacpd@yahoo.com>
2019-04-24 20:20:29 -0400

    push to concat

--- routes/questions.js
+++ routes/questions.js
@@ -57,7 +57,11 @@ router.post('/add', function (req, res) {
 	});
 	question.user = req.session.userId;
 
-	Media.find({_id: {$in: question.media}, used: {$eq: false}, user: {$eq: req.session.userId}}, function (err, media) {
+	Media.find({
+		_id: {$in: question.media},
+		used: {$eq: false},
+		user: {$eq: req.session.userId}
+	}, function (err, media) {
 		if (err) {
 			console.error(err.toString());
 			return res.status(404).json({status: "error", error: err.toString()});
@@ -92,7 +96,11 @@ router.post('/:id/answers/add', function (req, res) {
 		let answer = new Answer({body: req.body.body, media: req.body.media});
 		answer.question_id = question.id;
 		answer.user = req.session.username;
-		Media.find({_id: {$in: answer.media}, used: {$eq: false}, user: {$eq: req.session.userId}}, function (err, media) {
+		Media.find({
+			_id: {$in: answer.media},
+			used: {$eq: false},
+			user: {$eq: req.session.userId}
+		}, function (err, media) {
 			if (err) {
 				console.error(err.toString());
 				return res.status(404).json({status: "error", error: err.toString()});
@@ -153,16 +161,16 @@ router.delete('/:id', function (req, res) {
 					return console.error(err.toString());
 				}
 				//remove the answers associated with it
-				Answer.find({id: {$in : question.answers}}, function (err, answers) {
+				Answer.find({id: {$in: question.answers}}, function (err, answers) {
 					if (err) {
 						res.status(404).json({status: "error 404", error: err.toString()});
 						return console.error(err.toString());
 					}
 					let media_files = [];
-					answers.forEach((answer) => media_files.push(answer.media));
-					media_files.push(question.media);
+					answers.forEach((answer) => media_files.concat(answer.media));
+					media_files.concat(question.media);
 
-					Answer.deleteMany({id: {$in : question.answers}}, function(err){
+					Answer.deleteMany({id: {$in: question.answers}}, function (err) {
 						if (err) {
 							res.status(404).json({status: "error 404", error: err.toString()});
 							return console.error(err.toString());
@@ -170,7 +178,9 @@ router.delete('/:id', function (req, res) {
 						res.status(200).json({status: "OK"});
 					});
 					cassandra.execute(create_batch("DELETE", media_files));
-					Media.deleteMany({id: {$in : media_files}});
+					Media.deleteMany({id: {$in: media_files}}, function (err) {
+						if (err) console.log(err.toString());
+					});
 				});
 			});
 		}
